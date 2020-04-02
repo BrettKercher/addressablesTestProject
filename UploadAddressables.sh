ef672eb5ad930678ea1d03aeab9cb81706b76edf
Brett Kercher <brettkercher@utexas.edu>
2020-04-02 15:03:21 -0500

    blah

--- UploadAddressables.sh
+++ UploadAddressables.sh
@@ -1,4 +1,4 @@
-pathToAddressables=$WORKSPACE/.build/last/AB/extra_data/addrs/
+pathToAddressables=$WORKSPACE/.build/last/AB/extra_data/addressable_content/
 echo "Hello From Post Build Script!"
 ls $pathToAddressables
 echo "Hopefully that ls worked!"
