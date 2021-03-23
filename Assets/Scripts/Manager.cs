ee31aa15773aa97cc3110ac7fa54ae3d75b9b0b2
Brett Kercher <brettkercher@utexas.edu>
2021-03-23 10:55:26 -0500

    more local numbers

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,31 +6,32 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.ResourceManagement.ResourceLocations;
 
 public class Manager : MonoBehaviour {
-    public AssetReference localNumber;
-    public AssetLabelReference NumberLabel;
+    public AssetLabelReference RemoteNumberLabel;
+    public AssetLabelReference LocalNumberLabel;
     private List<IResourceLocation> _remoteNumbers;
+    private List<IResourceLocation> _localNumbers;
 
     private void Start() {
-        DisplayNumber();
-        Addressables.LoadResourceLocationsAsync(NumberLabel.labelString).Completed += LocationLoaded;
+        Addressables.LoadResourceLocationsAsync(LocalNumberLabel.labelString).Completed += LocalLocationLoaded;
+        Addressables.LoadResourceLocationsAsync(RemoteNumberLabel.labelString).Completed += RemoteLocationLoaded;
     }
 
-    private void DisplayNumber() {
-        // local prefab:
-        localNumber.InstantiateAsync(Vector3.zero, Quaternion.identity);
+    private void RemoteLocationLoaded(AsyncOperationHandle<IList<IResourceLocation>> obj) {
+        _localNumbers = new List<IResourceLocation>(obj.Result);
+        StartCoroutine(SpawnRemoteNumbers(3, _localNumbers));
     }
-
-    private void LocationLoaded(AsyncOperationHandle<IList<IResourceLocation>> obj) {
+    
+    private void LocalLocationLoaded(AsyncOperationHandle<IList<IResourceLocation>> obj) {
         _remoteNumbers = new List<IResourceLocation>(obj.Result);
-        StartCoroutine(SpawnRemoteNumbers());
+        StartCoroutine(SpawnRemoteNumbers(0, _remoteNumbers));
     }
 
-    private IEnumerator SpawnRemoteNumbers() {
+    private IEnumerator SpawnRemoteNumbers(int yOffset, IEnumerable<IResourceLocation> numbers) {
         yield return new WaitForSeconds(1f);
         var xOffset = -4.0f;
 
-        foreach (var num in _remoteNumbers) {
-            var spawnPosition = new Vector3(xOffset, 3, 0);
+        foreach (var num in numbers) {
+            var spawnPosition = new Vector3(xOffset, yOffset, 0);
             Addressables.InstantiateAsync(num, spawnPosition, Quaternion.identity);
             xOffset += 2.5f;
             yield return new WaitForSeconds(1f);
